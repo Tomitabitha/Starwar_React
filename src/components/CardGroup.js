a23081d7d977c5e42eb4a302ac923bb7123123e7
Ndibechinonso <edwardndibe@gmail.com>
2023-04-28 09:20:13 +0100

    added key for list

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -4,7 +4,7 @@ import Card from './Card'
 const CardGroup = ({movies}) => {
   return (
     <div className='data-grid'>
-         {movies.map(movie => <Card list={movie} /> 
+         {movies.map(movie => <Card key={movie.episode_id} list={movie} /> 
         )} 
         </div>
   )
